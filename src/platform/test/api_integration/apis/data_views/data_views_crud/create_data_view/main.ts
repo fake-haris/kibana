68ef79f8dbadb1a23fa4c53209a2e1466877e8f8
Charis Kalpakis <39087493+fake-haris@users.noreply.github.com>
2026-02-17 15:40:00 +0200

    [Scout] Migrate Data Views API tests from FTR - Part1 (#252122)
    (cherry picked from commit 4d5391ba18c8fbf011101ee05b6dfd9103be36bb)
    
    # Conflicts:
    #	.buildkite/scout_ci_config.yml
    #	src/platform/plugins/shared/data_views/test/scout/api/playwright.config.ts
    #	src/platform/plugins/shared/data_views/test/scout/api/tests/data_views/crud_delete_errors.spec.ts
    #	src/platform/test/api_integration/apis/data_views/data_views_crud/create_data_view/main.ts
    #	src/platform/test/api_integration/apis/data_views/data_views_crud/create_data_view/validation.ts

--- src/platform/test/api_integration/apis/data_views/data_views_crud/create_data_view/main.ts
+++ src/platform/test/api_integration/apis/data_views/data_views_crud/create_data_view/main.ts
@@ -7,7 +7,7 @@
  * License v3.0 only", or the "Server Side Public License, v 1".
  */
 
-import expect from '@kbn/expect';
+import expect from '@kbn/expect/expect';
 import { FtrProviderContext } from '../../../../ftr_provider_context';
 import { configArray, dataViewConfig } from '../../constants';
 
